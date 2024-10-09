ee3debe7227136d4ba9f950f6f4a9f6472b9b4ed
yisaan <aevron10@gmail.com>
2024-10-09 12:44:20 -0700

    part 5 done

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -44,7 +44,7 @@ const findUserByName = (name) => {
     return users["users_list"].filter(
       (user) => user["name"] === name
     );
-  };
+};
   
 app.get("/users", (req, res) => {
     const name = req.query.name;
@@ -54,6 +54,19 @@ app.get("/users", (req, res) => {
         res.send(result);
     } else {
         res.send(users);
+    }
+});
+
+const findUserById = (id) =>
+    users["users_list"].find((user) => user["id"] === id);
+  
+app.get("/users/:id", (req, res) => {
+    const id = req.params["id"]; //or req.params.id
+    let result = findUserById(id);
+    if (result === undefined) {
+        res.status(404).send("Resource not found.");
+    } else {
+        res.send(result);
     }
 });
 
